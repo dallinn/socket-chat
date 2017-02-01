b50bf9175cc4b368877caab9caff9e3017fdd032
dallinn <dallinnickle@gmail.com>
2017-02-01 09:24:27 -0700

    reorganize and add user tracking functionality

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use('/style.css', express.static(__dirname + '/style.css'));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    socket.on('username', function(usr){
+        socket['username'] = usr;
+        console.log(socket['username']);
+    });
+    socket.on('chat message', function(msg){
+        io.emit('chat message',{ message: msg, username: socket['username'] });
+    });
+});
+
+http.listen(3000);
