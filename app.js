18b4edc23068ece3f5cc6fbc705b0a8f02797967
dallinn <dallinnickle@gmail.com>
2017-02-01 10:37:36 -0700

    add client list

--- app.js
+++ app.js
@@ -9,14 +9,22 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
+var clients = [];
+
 io.on('connection', function(socket){
     socket.on('username', function(usr){
+        clients.push(usr);
         socket['username'] = usr;
         socket.emit('username', socket['username']);
-        //console.log(socket['username']);
     });
     socket.on('chat message', function(msg){
         io.emit('chat message',{ message: msg, username: socket['username'] });
+    });
+    socket.on('getClients', function(){
+        console.log(clients);
+    });
+    socket.on('disconnect', function() {
+        clients.splice(clients.indexOf(socket['username']), 1);
     });
 });
 
